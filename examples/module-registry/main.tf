5eb9322ce18c5470ec98e4b1135961e05e1a9a52
markti <markti@outlook.com>
2024-07-03 08:55:02 -0400

    add module registry sample

--- examples/module-registry/main.tf
+++ examples/module-registry/main.tf
@@ -1,7 +1,3 @@
-module "regions" {
-  source  = "Azure-Terraformer/regions/azurerm"
-  version = "1.0.1"
-}
 
 provider "azurerm" {
   features {}
--- examples/module-registry/variables.tf
+++ examples/module-registry/variables.tf
@@ -2,7 +2,12 @@ variable "primary_region" {
   type = string
 
   validation {
-    condition     = contains(module.regions.supported_geographies, var.primary_region)
+    condition     = contains(module.regions.all_region_names, var.primary_region)
     error_message = "The primary region must be a supported region."
   }
+}
+
+module "regions" {
+  source  = "Azure-Terraformer/regions/azurerm"
+  version = "1.0.2"
 }
